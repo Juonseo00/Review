0f92a5dc377dcd99a78f1c3c939568c503dac5e3
junseo <junseo129@naver.com>
2022-05-28 09:10:55 +0900

    회원가입

---
+++ testfile.py
@@ -0,0 +1,8 @@
+input_id = input("아이디를 입력해주세요\n")
+input_pwd = input("비밀번호를 입력해주세요 : \n")
+real_id = "junseo"
+real_pwd = "yangjunseo"
+if real_id == input_id and real_pwd == input_pwd : 
+        print("Hello" + " " + input_id)
+else :
+      print("아이디 혹은 비밀번호를 확인하세요") 
